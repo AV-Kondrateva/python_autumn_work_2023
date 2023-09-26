711b0875148045238ca8ae3d7c5321c9bc4d367d
AV-Kondrateva <a.v.kondrateva@mail.ru>
2023-09-27 01:07:39 +0300

    Initial Commit

---
+++ Task 4-1.py
@@ -0,0 +1,13 @@
+x,operat,y = (int(input()), input(), int(input()))
+if operat == '+':
+    a = x+y
+    print (x,'+',y,'=',a)
+elif operat == '*':
+        a = x * y
+        print(x, '*', y, '=', a)
+elif operat == '/':
+    a = x / y
+    print(x, '/', y, '=', a)
+elif operat == '-':
+    a = x - y
+    print(x, '-', y, '=', a)
